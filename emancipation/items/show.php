a6b7e7705907351cf4f437e5b9a622730651be62
Jason Roma <jasonnroma@gmail.com>
2014-06-30 15:37:20 -0400

    Moved thumbnails above Dublin Core fields.

--- emancipation/items/show.php
+++ emancipation/items/show.php
@@ -2,8 +2,6 @@
 
 <h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 
-<?php echo all_element_texts('item'); ?>
-
 <!-- The following returns all of the files associated with an item. -->
 <?php if (metadata('item', 'has files')): ?>
 <div id="itemfiles" class="element">
@@ -15,6 +13,8 @@
     <?php endif; ?>
 </div>
 <?php endif; ?>
+
+<?php echo all_element_texts('item'); ?>
 
 <!-- If the item belongs to a collection, the following creates a link to that collection. -->
 <?php if (metadata('item', 'Collection Name')): ?>
