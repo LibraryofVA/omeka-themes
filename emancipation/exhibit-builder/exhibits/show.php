c1ae1dfcf9e486f476891030dde9a48ef6cd93b8
jasonroma <jasonnroma@gmail.com>
2014-07-18 11:30:41 -0400

    Removed on page navigation related to children

--- emancipation/exhibit-builder/exhibits/show.php
+++ emancipation/exhibit-builder/exhibits/show.php
@@ -3,17 +3,8 @@ echo head(array(
     'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
     'bodyclass' => 'exhibits show'));
 ?>
-<!--
-<nav id="exhibit-pages">
-    <?php echo exhibit_builder_page_nav(); ?>
-</nav>
--->
 <?php set_current_record('exhibit_page', $exhibit_page); ?>
 <h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
-
-<nav id="exhibit-child-pages">
-    <?php echo exhibit_builder_child_page_nav(); ?>
-</nav>
 
 <?php exhibit_builder_render_exhibit_page(); ?>
 
