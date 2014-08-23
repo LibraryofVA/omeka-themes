522238000308aaa5693b2535539f502bc51101a6
jasonroma <jasonnroma@gmail.com>
2014-08-23 11:56:35 -0400

    Rework of Header and Footer

--- scribe/common/footer.php
+++ scribe/common/footer.php
@@ -1,10 +1,49 @@
 
-</div><!-- end content -->
-<?php $base_dir = basename(getcwd()); ?>
-<div id="footer">
-<div class="footer">
-<a href="/<?php echo $base_dir; ?>"><img src="<?php echo img('sub.png'); ?>" alt="Scribe: an Omeka theme" title="Scribe: an Omeka theme" width="960" height="80" border="0"></a>
-</div><!-- end footer -->
-</div><!-- end wrap -->
+        </div><!-- end content -->
+        <?php $base_dir = basename(getcwd()); ?>
+        <div id="footer">
+           <hr />
+           <div class="footer">
+              <div class="row2">
+              <div class="span3-linda bar-linda about">
+                <h3>About</h3>
+                <ul>
+                  <li><a href="http://www.virginiamemory.com/transcribe/about">About the project</a></li>
+                  <li><a href="http://www.virginiamemory.com/transcribe/about#faq">FAQ</a></li>
+                  <li><a href="http://www.virginiamemory.com/transcribe/about#tips">Transcription tips</a></li>
+                  <li><a href="mailto:makinghistory@virginiamemory.com">Contact us</a></li>
+                </ul>
+                <h3>Contribute</h3>
+                <ul>
+                  <li><?php echo countFinished(); ?> pages transcribed</li>
+                  <li><a href="/transcribe/code">Get the code</a></li>
+                </ul>
+              </div>
+              <div class="span3-linda bar-linda connect">
+                <h3>Virginia Memory</h3>
+                <div>Making History - Transcribe is a digital collection based project part of <a href="/" title="Virginia Memory">Virginia Memory</a>, the <a href="http://www.lva.virginia.gov" title="Library of Virginia" target="_blank">Library of Virginia</a>'s home for digital collections.</div>
+                <br>
+                <h3>Connect With Us</h3>
+                <div id="connected">
+			        <a title="Join us on Facebook" href="http://www.facebook.com/LibraryofVA"><img src="/img/hp_facebook.png"></a> <a title="Follow us on Twitter" href="http://twitter.com/LibraryofVA"><img src="/img/hp_twitter.png"></a> <a title="Check out our Flickr Photostream" href="http://www.flickr.com/photos/library_of_virginia/"><img src="/img/hp_flickr.png"></a> <a title="Subscribe to one of our RSS Feeds" href="http://www.virginiamemory.com/about/news_feedsv"><img src="/img/hp_rss.png"></a> <a title="View our YouTube Channel" href="http://www.youtube.com/user/LibraryofVa"><img src="/img/hp_youtube.png"></a> <a target="_blank" title="Historypin Channel" href="http://www.historypin.com/channels/view/id/8307088/"><img alt="Historypin Channel" src="/img/hp_historypin.png"></a> <a target="_blank" title="Tumblr" href="http://libraryofva.tumblr.com/"><img alt="Tumblr" src="/img/hp_tumblr.png"></a> <a target="_blank" title="Pinterest" href="http://pinterest.com/libraryofva/"><img alt="Pinterest" src="/img/hp_pinterest.png"></a>  <a target="_blank" title="Instagram" href="http://instagram.com/libraryofva"><img alt="Instagram" src="/img/hp_instagram.png"></a>
+		        </div>
+              </div>
+              <div class="span3 twitter" data-twttr-id="twttr-sandbox-0" style="width:440px;">
+                <a class="twitter-timeline"  href="https://twitter.com/LibraryofVA"  data-widget-id="414460525192294402">Tweets by @LibraryofVA</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+              </div>
+            </div>
+        </div><!-- end footer -->
+    </div><!-- end wrap -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53957555-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
--- scribe/common/header.php
+++ scribe/common/header.php
@@ -1,57 +1,76 @@
 <!DOCTYPE html>
 <html lang="<?php echo get_html_lang(); ?>">
 <head>
-<script type="text/javascript"> </script>
-<meta charset="utf-8">
-<?php if ( $description = settings('description')): ?>
-<meta name="description" content="<?php echo $description; ?>" />
-<?php endif; ?>
-<title><?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
-<?php echo auto_discovery_link_tags(); ?>
-<!-- Plugin Stuff -->
-<?php plugin_header(); ?>
-<!-- Le styles -->
-<link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
-<link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
-<?php
-queue_css('bootstrap');
-queue_css('font-awesome');
-queue_css('style');
-display_css();
-$js_dir = 'http://'.$_SERVER['HTTP_HOST'] . '/' . basename(getcwd()) . '/themes/scribe/javascripts/';
-?>
-<!-- JavaScripts -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" charset="utf-8"></script>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" charset="utf-8"></script>
-<script type="text/javascript" src="<?php echo $js_dir; ?>bootstrap.min.js" charset="utf-8"></script>
-<script type="text/javascript" src="<?php echo $js_dir; ?>jquery.bxSlider.min.js" charset="utf-8"></script>
+	<script type="text/javascript"> </script>
+    <meta charset="utf-8">
+    <?php if ( $description = settings('description')): ?>
+    <meta name="description" content="<?php echo $description; ?>" />
+    <?php endif; ?>
+
+    <title><?php echo settings('site_title'); echo isset($title) ? ' | ' . strip_formatting($title) : ''; ?></title>
+
+    <?php echo auto_discovery_link_tags(); ?>
+
+    <!-- Plugin Stuff -->
+    <?php plugin_header(); ?>
+
+	<!-- Le styles -->
+    <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+	
+    <?php
+	queue_css('bootstrap');
+	queue_css('font-awesome');
+    queue_css('style');
+	queue_css('ol');
+	display_css();
+    $js_dir =  'http://'.$_SERVER['HTTP_HOST'] . '/' . basename(getcwd()) . '/themes/scribe/javascripts/';
+	?>
+    
+    <!-- JavaScripts -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="<?php echo $js_dir; ?>bootstrap.min.js" charset="utf-8"></script>
+    <script type="text/javascript" src="<?php echo $js_dir; ?>jquery.bxSlider.min.js" charset="utf-8"></script>
+
 </head>
-<?php
-echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass));
-$base_dir = basename(getcwd());
-require_once getcwd().'/plugins/Scripto/libraries/Scripto.php';
-$scripto = ScriptoPlugin::getScripto();
-?>
-<div class="container">
-<div id="sublinks" class="masthead clearfix">
-<div id="header">
-<ul class="nav nav-pills pull-right">
-<?php if ($scripto->isLoggedIn()): ?>
-<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><strong><?php echo $scripto->getUserName(); ?></strong><b class="caret"></b></a>
-<ul class="dropdown-menu">
-<li><a href="/<?php echo $base_dir; ?>/scripto">Your Contributions</a></li>
-<li><a href="/<?php echo $base_dir; ?>/scripto/watchlist">Your Watchlist</a></li>
-<li><a href="/<?php echo $base_dir; ?>/scripto/recent-changes">Recent Changes</a></li>
-<li><a href="/<?php echo $base_dir; ?>/scripto/logout">Logout</a></li>
-</ul>
-</li>
-<?php else: ?>
-<li>
-<a href="/<?php echo $base_dir; ?>/scripto/login"><strong>Sign in or register</strong></a>
-</li>
-<?php endif; ?>
-</ul>
-<a href="/<?php echo $base_dir; ?>"><img src="<?php echo img('sub.png'); ?>" alt="Scribe: an Omeka theme" title="Scribe: an Omeka theme" width="960" height="80" border="0"></a>
-</div>
+
+<?php 
+    echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); 
+    $base_dir = basename(getcwd());
+    require_once getcwd().'/plugins/Scripto/libraries/Scripto.php';
+    $scripto = ScriptoPlugin::getScripto();
+?>  
+
+    <div class="container">
+
+        <div id="sublinks" class="masthead clearfix">
+
+            <div id="header">
+    
+                <ul class="nav nav-pills pull-right">
+
+                    <?php if ($scripto->isLoggedIn()): ?>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown"><strong><?php echo $scripto->getUserName(); ?></strong><b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="/<?php echo $base_dir; ?>/scripto">Your Contributions</a></li>
+                            <li><a href="/<?php echo $base_dir; ?>/scripto/watchlist">Your Watchlist</a></li>
+                            <li><a href="/<?php echo $base_dir; ?>/scripto/recent-changes">Recent Changes</a></li>
+                            <li><a href="/<?php echo $base_dir; ?>/scripto/logout">Logout</a></li>
+                        </ul>
+                    </li>
+
+                    <?php else: ?>
+
+                    <li>
+                    <a href="/<?php echo $base_dir; ?>/scripto/login"><strong>Sign in or register</strong></a>                          
+                    </li>
+
+                    <?php endif; ?>
+                </ul>
+    
+            <a href="/<?php echo $base_dir; ?>"><img src="<?php echo img('sub.png'); ?>" alt="Scribe: an Omeka theme" title="Scribe: an Omeka theme" width="939" height="188" border="0"></a>
+      </div>           
+
 </div><!-- end header -->
-<br /><br />
+<hr>
