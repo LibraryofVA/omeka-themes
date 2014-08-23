2ca75db3baffab8035258be17766e518a67d716d
jasonroma <jasonnroma@gmail.com>
2014-08-23 11:47:38 -0400

    Scribe Theme Initial Upload

---
+++ scribe/common/footer.php
@@ -0,0 +1,10 @@
+
+</div><!-- end content -->
+<?php $base_dir = basename(getcwd()); ?>
+<div id="footer">
+<div class="footer">
+<a href="/<?php echo $base_dir; ?>"><img src="<?php echo img('sub.png'); ?>" alt="Scribe: an Omeka theme" title="Scribe: an Omeka theme" width="960" height="80" border="0"></a>
+</div><!-- end footer -->
+</div><!-- end wrap -->
+</body>
+</html>
